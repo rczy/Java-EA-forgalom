2815aaf6a65e05365893e48a51823caeee697f96
SteveDMC <dmc120@gmail.com>
2023-11-27 15:27:31 +0100

    Döntési-fa frissítés

--- src/main/java/app/gui/adatbanyaszat/DontesiFaController.java
+++ src/main/java/app/gui/adatbanyaszat/DontesiFaController.java
@@ -11,6 +11,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import weka.classifiers.trees.J48;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class DontesiFaController {
     @FXML
     private void indit(ActionEvent event) {
@@ -18,20 +20,25 @@ public class DontesiFaController {
         Button button = (Button)event.getSource();
         button.setDisable(true);
         ForgalomApplication.getStage().getScene().getRoot().setCursor(Cursor.WAIT);
+        AtomicBoolean success = new AtomicBoolean();
         new Thread(() -> {
             try {
-                GepiTanulas gt = new GepiTanulas("data/diabetes.arff", 8, new J48());
+                GepiTanulas gt = new GepiTanulas("data/diabetes.arff", 8, new J48(), false);
                 gt.printSummary(true, file);
+                success.set(true);
             } catch (Exception e) {
+                success.set(false);
                 Platform.runLater(() -> {
                     Alert alert = new Alert(Alert.AlertType.ERROR, e.getMessage(), ButtonType.OK);
                     alert.show();
                 });
             } finally {
                 Platform.runLater(() -> {
+                    if (success.get()) {
+                        Alert alert = new Alert(Alert.AlertType.INFORMATION, "Az eredmény kiírásra került a(z) " + file + " fájlba.", ButtonType.OK);
+                        alert.show();
+                    }
                     ForgalomApplication.getStage().getScene().getRoot().setCursor(Cursor.DEFAULT);
-                    Alert alert = new Alert(Alert.AlertType.INFORMATION, "Az eredmény kiírásra került a(z) " + file + " fájlba.", ButtonType.OK);
-                    alert.show();
                     button.setDisable(false);
                 });
             }
